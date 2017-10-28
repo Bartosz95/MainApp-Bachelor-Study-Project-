9361622e3b99af6c314654c14d09e803caae1e04
pizero <218518@student.pwr.edu.pl>
2017-10-28 19:54:31 +0200

    wlasciwe wysylanie

--- rasp0_cam/main.py
+++ rasp0_cam/main.py
@@ -22,57 +22,48 @@ def recording_video():
     print("recording succesfully!")
     return video_data
 
-def server_connect(video_data, remotepath):
-    end_of_path = '/VIDEO/' + video_data.strftime("%y.%m.%d") + '/' + user_conf_file.cam_name + \
-				  '/' + video_data.strftime(user_conf_file.data_format) + '.' + user_conf_file.video_format
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=user_conf_file.server_hostname,
-                port=user_conf_file.server_port, 
-                username=user_conf_file.server_username, 
-                password=user_conf_file.server_password)
-    ssh.close()
-    print("connect succesfully!")
-
-
+# tworzenie sciezki na serwerze
 def mkdir_server(sftp,directory):
     try:
-        sftp.mkdir(directory)
+        sftp.chdir(directory)
     except IOError:
-        head, tail = os.path.split(directory)
-        print(head)
-        print (tail)
-        mkdir_server(sftp,head)
-        sftp.mkdir(directory)
+        try:
+          sftp.mkdir(directory)
+        except IOError:
+            head, tail = os.path.split(directory)
+            mkdir_server(sftp,head)
+            sftp.mkdir(directory)
+
+
+
+def send_to_server(video_data):
+
+    transport = paramiko.Transport((user_conf_file.server_hostname, user_conf_file.server_port))
+    transport.connect(username=user_conf_file.server_username, password=user_conf_file.server_password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+
+    end_of_path = '/VIDEO/' + video_data.strftime("%y.%m.%d") + '/' + user_conf_file.cam_name + \
+				  '/' + video_data.strftime(user_conf_file.data_format) + '.' + user_conf_file.video_format
+
+    dir_path = os.path.dirname(user_conf_file.video_path + end_of_path)
+
+    mkdir_server(sftp, dir_path)
+
+    sftp.put(os.path.abspath('.') + end_of_path, user_conf_file.video_path + end_of_path)
+
+    sftp.close()
+    transport.close()
+    print("send succesfully!")
+
+
+
 
 
 
 try:
     video_data = recording_video()
-    end_of_path = '/VIDEO/' + video_data.strftime("%y.%m.%d") + '/' + user_conf_file.cam_name + \
-                  '/' + video_data.strftime(user_conf_file.data_format) + '.' + user_conf_file.video_format
+    send_to_server(video_data)
 
-    transport = paramiko.Transport((user_conf_file.server_hostname, user_conf_file.server_port))
-    transport.connect(username = user_conf_file.server_username, password = user_conf_file.server_password)
-    sftp = paramiko.SFTPClient.from_transport(transport)
-
-    dir_path = os.path.dirname(user_conf_file.video_path + end_of_path)
-
-    print(dir_path)
-    mkdir_server(sftp,dir_path)
-
-    """
-    try:
-        sftp.chdir(dir_path)
-    except IOError:
-        sftp.mkdir(dir_path)
-        sftp.chdir(dir_path)
-
-    #sftp.put(os.path.abspath('.') + end_of_path, remotepath + end_of_path)
-
-    transport.close()
-    sftp.close()"""
-
-#server_connect(video_data, user_conf_file.video_path)
 except Exception as ex:
     print(ex)
