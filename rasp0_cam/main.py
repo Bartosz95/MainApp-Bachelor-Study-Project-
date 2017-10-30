e1464e00dbe8cb2b11330484d08344258d52bb4f
pizero <218518@student.pwr.edu.pl>
2017-10-30 18:02:41 +0100

    zatrzymanie zerem

--- rasp0_cam/main.py
+++ rasp0_cam/main.py
@@ -12,34 +12,52 @@ import posixMutexFile
 
 
 MUTEX = ""
+START = True
+
+
+def start_recording(camera,video_data):
+    global MUTEX
+    path_to_video = os.path.abspath('.') + '/VIDEO/' + video_data.strftime("%y.%m.%d") + \
+                         '/' + user_conf_file.cam_name + '/' + \
+                         video_data.strftime(user_conf_file.data_format) + '.' + \
+                         user_conf_file.video_format
+    MUTEX = path_to_video
+    if os.path.isdir(os.path.dirname(path_to_video)) == False: os.makedirs(os.path.dirname(path_to_video))
+
+    camera.resolution = (user_conf_file.video_resolution_x, user_conf_file.video_resolution_y)
+    camera.start_recording(path_to_video)
+    print("start ------ ", path_to_video)
+
+
+def stop_recording(camera):
+    global MUTEX
+    camera.stop_recording()
+    MUTEX = ""
+    print("stop recordking")
+
 
 # nagrywanie filmu i zapisywanie w ./VIDEO/
 class recording_video(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        global START
         self.camera = picamera.PiCamera()
+        self.video_data = datetime.datetime.now()
     def run(self):
-        global MUTEX
-        global MUTEX_VIDEO_DIR
-        while 1:
+
+        self.video_data = datetime.datetime.now()
+        start_recording(self.camera, self.video_data)
+
+        while START:
             try:
-                video_data = datetime.datetime.now()
-
-                path_to_video = os.path.abspath('.') + '/VIDEO/' + video_data.strftime("%y.%m.%d") + '/' + user_conf_file.cam_name + \
-                                '/' + video_data.strftime(user_conf_file.data_format) + '.' + user_conf_file.video_format
-
-                if os.path.isdir(os.path.dirname(path_to_video)) == False: os.makedirs(os.path.dirname(path_to_video))
-
-                self.camera.resolution = (user_conf_file.video_resolution_x, user_conf_file.video_resolution_y)
-
-                MUTEX = path_to_video
-                self.camera.start_recording(path_to_video)
-                self.camera.wait_recording(user_conf_file.video_time)
-                self.camera.stop_recording()
-                MUTEX = ""
-                print("recording --",path_to_video)
+                if self.video_data + datetime.timedelta(seconds=user_conf_file.video_time) < datetime.datetime.now():
+                    stop_recording(self.camera)
+                    self.video_data = datetime.datetime.now()
+                    start_recording(self.camera, self.video_data)
             except Exception as ex:
                  print(ex)
+            time.sleep(2)
+        stop_recording(self.camera)
 
 
 # tworzenie sciezki na serwerze (funkcja rekurencyjna)
@@ -71,6 +89,8 @@ def send_file(file_path):
             sftp.close()
             transport.close()
             print("send ------- ", file_path)
+
+
         except Exception as ex:
             raise ex
     else:
@@ -81,11 +101,11 @@ def send_dir(dir_path):
 
     try:
         global MUTEX
-        global MUTEX_VIDEO_DIR
-
+        global START
         if os.path.isdir(dir_path):
             dir_list = os.listdir(dir_path)
             for new in dir_list:
+                if START == False : break
                 new_dir_path = dir_path + '/' + new
                 send_dir(new_dir_path)
         if os.path.isfile(dir_path):
@@ -93,7 +113,7 @@ def send_dir(dir_path):
                 send_file(dir_path)
                 os.unlink(dir_path) # usuwanie wyslanego pliku
             else:
-                print("MUTEX")
+                print("MUTEX ------ ",dir_path)
     except Exception as ex:
         raise ex
 
@@ -101,15 +121,16 @@ def send_dir(dir_path):
 class send_video_to_server(threading.Thread):
     def __init__(self,path):
         threading.Thread.__init__(self)
+        global START
         self.dir_path = path
     def run(self):
-        while 1 :
+        while START :
             try:
                 send_dir(self.dir_path)
             except Exception as ex:
                 print(ex)
             print("all send")
-            time.sleep(user_conf_file.video_time)
+            time.sleep(user_conf_file.video_time*0.5)
 
 
 try:
@@ -118,6 +139,12 @@ try:
 
     thread_record.start()
     thread_send.start()
+
+    while START:
+        x = int(input())
+        if x == 0:
+            START = False
+            break
 
 except Exception as ex:
     print(ex)
