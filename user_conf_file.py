9fcac55b2ad113c4a1a3b9c3ac42c5458334d47f
root_pi0 <218518@student.pwr.edu.pl>
2017-11-08 18:52:29 +0100

    zmiana formatu na mp4

--- user_conf_file.py
+++ user_conf_file.py
@@ -5,11 +5,14 @@ server_port = 22
 server_username = 'pi'
 server_password = 'mampiernik'
 
-video_path = '/home/pi/Videos/MONITORING'  # zdefiniowana sciezka serveru
+video_path = '/home/pi/Videos/MONITORING'  # zdefiniowana sciezka na serwerze
+video_path_on_camera = "/home/pi/Documents/inzynierka/rasp0_cam"  # zdefiniowana sciezka na kamerze
 video_format = 'h264'  # rozszerzenie pliku np h264
-video_time = 1  # czas nagrywania w sekundach
+video_time = 3  # czas nagrywania w sekundach
 video_resolution_x = 640
 video_resolution_y = 480
+video_framerate = 30
+video_start_stop = True
 
 cam_name = 'CAM1'  # nazwa kamery
-data_format = "%y.%m.%d %H:%M:%S" # format daty (nazwy
+data_format = "%y-%m-%d_%H.%M.%S" # format daty (nazwy
